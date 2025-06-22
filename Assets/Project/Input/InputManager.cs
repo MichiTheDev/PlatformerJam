7d23893b39144b8e3262d094ea9ff6568c5f232d
EinfachMichi <158079374+MichiTheDev@users.noreply.github.com>
2025-06-22 14:38:33 +0200

    Initial commit

---
+++ Assets/Project/Input/InputManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public sealed class InputManager : MonoBehaviour
+{
+    public static InputManager Instance { get; private set; }
+    public static GameInput Input { get; private set; }
+    
+    private void Awake()
+    {
+        if(Instance is not null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        Input = new GameInput();
+        Input.Enable();
+    }
+}
