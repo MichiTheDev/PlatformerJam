65b04a23a383702c0299479dcf70d0492640c1f0
EinfachMichi <158079374+MichiTheDev@users.noreply.github.com>
2025-06-23 21:39:31 +0200

    PlayerMovement-V0.0.3

--- Assets/Project/Scripts/Player/PlayerAnimator.cs
+++ Assets/Project/Scripts/Player/PlayerAnimator.cs
@@ -6,8 +6,15 @@
 )]
 public sealed class PlayerAnimator : MonoBehaviour
 {
+	[SerializeField] private float _pushRunAnimationSpeed = 1.5f;
+	[SerializeField] private Vector2 _pushAnimationColliderOffset;
+	[SerializeField] private Vector2 _pushAnimationColliderSize;
+	
 	private Animator _animator;
 	private PlayerMovement _playerMovement;
+
+	private Vector2 _defaultColliderOffset;
+	private Vector2 _defaultColliderSize;
 	
 	private void Awake()
 	{
@@ -18,19 +25,23 @@ public sealed class PlayerAnimator : MonoBehaviour
 	private void Start()
 	{
 		_playerMovement.OnJumpStarted += () => _animator.SetTrigger("Jump");
-		_playerMovement.OnRunStateChanged += running => _animator.SetBool("IsRunning", running);
+		_playerMovement.OnRunStateChanged += OnRunStateChanged;
+		_playerMovement.OnObstacleTouched += OnObstacleTouched;
+		_playerMovement.OnObstacleReleased += OnObstacleReleased;
 		
 		GravityManager.Instance.OnGravityDirectionChanged += OnGravityDirectionChanged;
+
+		_defaultColliderSize = _playerMovement.Collider.size;
+		_defaultColliderOffset = _playerMovement.Collider.offset;
 	}
 
 	private void OnDestroy()
 	{
+		_playerMovement.OnRunStateChanged -= OnRunStateChanged;
+		_playerMovement.OnObstacleTouched -= OnObstacleTouched;
+		_playerMovement.OnObstacleReleased -= OnObstacleReleased;
+		
 		GravityManager.Instance.OnGravityDirectionChanged -= OnGravityDirectionChanged;
-	}
-
-	private void OnGravityDirectionChanged(GravityDirection gravityDirection)
-	{
-		_animator.SetInteger("GravityDirection", gravityDirection == GravityDirection.Down ? -1 : 1);
 	}
 
 	private void Update()
@@ -38,5 +49,44 @@ public sealed class PlayerAnimator : MonoBehaviour
 		_animator.SetFloat("YVelocity", _playerMovement.VerticalVelocity);
 		_animator.SetFloat("Speed", Mathf.Abs(_playerMovement.HorizontalVelocity));
 		_animator.SetBool("IsGrounded", _playerMovement.IsGrounded);
+		_animator.SetBool("IsObstacleInFront", _playerMovement.IsObstacleInFront);
+	}
+	
+	private void OnGravityDirectionChanged(GravityDirection gravityDirection)
+	{
+		_animator.SetInteger("GravityDirection", gravityDirection == GravityDirection.Down ? -1 : 1);
+	}
+
+	private void OnRunStateChanged(bool running)
+	{
+		_animator.SetBool("IsRunning", running);
+
+		if(_playerMovement.IsObstacleInFront && running)
+		{
+			_animator.speed = _pushRunAnimationSpeed;
+		}
+		else
+		{
+			_animator.speed = 1f;
+		}
+	}
+	
+	private void OnObstacleTouched(GameObject obstacle)
+	{
+		_playerMovement.Collider.size = _pushAnimationColliderSize;
+		_playerMovement.Collider.offset = _pushAnimationColliderOffset;
+
+		if(_playerMovement.IsRunning)
+		{
+			_animator.speed = _pushRunAnimationSpeed;
+		}
+	}
+	
+	private void OnObstacleReleased(GameObject obstacle)
+	{
+		_playerMovement.Collider.size = _defaultColliderSize;
+		_playerMovement.Collider.offset = _defaultColliderOffset;
+		
+		_animator.speed = 1f;
 	}
 }
--- Assets/Project/Scripts/Player/PlayerMovement.cs
+++ Assets/Project/Scripts/Player/PlayerMovement.cs
@@ -3,18 +3,27 @@ using UnityEngine;
 
 [RequireComponent(
     typeof(Rigidbody2D),
-    typeof(Collider2D)
+    typeof(BoxCollider2D)
 )]
 public sealed class PlayerMovement : MonoBehaviour
 {
     public event Action OnJumpStarted;
+    public event Action OnLanded;
+    public event Action<GameObject> OnObstacleTouched;
+    public event Action<GameObject> OnObstacleReleased;
     public event Action<bool> OnRunStateChanged;
     
     public float HorizontalVelocity => _rb.linearVelocityX;
     public float VerticalVelocity => _rb.linearVelocityY;
     
+    public GameObject Obstacle { get; private set; }
+    public BoxCollider2D Collider { get; private set; }
+    
+    public float FacingDirection { get; private set; }
+    
     public bool IsRunning { get; private set; }
     public bool IsGrounded { get; private set; }
+    public bool IsObstacleInFront { get; private set; }
 
     [Header("Movement")]
     [SerializeField] private float _walkSpeed = 3f;
@@ -31,25 +40,31 @@ public sealed class PlayerMovement : MonoBehaviour
     [Header("Ground Check")]
     [SerializeField] private LayerMask _excludedGroundCheckLayers;
     [SerializeField] private float _groundCheckHeight = 0.05f;
+
+    [Header("Obstacle Check")] 
+    [SerializeField] private LayerMask _obstacleLayer;
+    [SerializeField] private float _obstacleCheckDistance = 0.1f;
+    [Tooltip("This value will be multiplied by the height of the collider to determine the check origin. " +
+             "I.e. if the value is 0.8f, it will be at 80% of the characters height, which could be around the head.")]
+    [SerializeField, Range(0.01f, 1f)] private float _obstacleCheckHeight = 0.8f;
     
     private Rigidbody2D _rb;
-    private Collider2D _collider;
     
     private float _movementSpeed;
     private float _movementInput;
     private float _jumpStartY;
     private float _coyoteTimer;
+    private float _flipY = 1f;
 
     private bool _isJumping;
     private bool _jumpHeld;
 
     private float _gravity => Mathf.Abs(Physics2D.gravity.y * _rb.gravityScale);
-    private float _yDirection => transform.localScale.y;
     
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _collider = GetComponent<Collider2D>();
+        Collider = GetComponent<BoxCollider2D>();
     }
 
     private void Start()
@@ -60,6 +75,7 @@ public sealed class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         UpdateGroundCheck();
+        UpdateObstacleCheck();
         UpdateMovement();
         UpdateJump();
     }
@@ -91,7 +107,7 @@ public sealed class PlayerMovement : MonoBehaviour
         _jumpHeld = true;
         _jumpStartY = transform.position.y;
         
-        _rb.linearVelocityY = Mathf.Sqrt(2f * _gravity * _maxJumpHeight) * _yDirection;
+        _rb.linearVelocityY = Mathf.Sqrt(2f * _gravity * _maxJumpHeight) * _flipY;
         OnJumpStarted?.Invoke();
     }
 
@@ -102,53 +118,20 @@ public sealed class PlayerMovement : MonoBehaviour
 
     public void FlipX(bool flip)
     {
-        transform.localScale = new Vector3(flip ? -1f : 1f, transform.localScale.y, 1f);
+        FacingDirection = flip ? -1f : 1f;
+        transform.localScale = new Vector3(FacingDirection, _flipY, 1f);
     }
 
     public void FlipY(bool flip)
     {
-        transform.localScale = new Vector3(transform.localScale.x, flip ? -1f : 1f, 1f);
-    }
-    
-    private void UpdateMovement()
-    {
-        _rb.linearVelocityX = _movementInput * _movementSpeed;
-    }
-
-    private void UpdateJump()
-    {
-        if(!_isJumping) return;
-
-        float jumpHeightSinceStart = transform.position.y - _jumpStartY * _yDirection;
-        if(jumpHeightSinceStart >= _minJumpHeight)
-        {
-            if (!_jumpHeld)
-            {
-                _rb.linearVelocityY *= _jumpEndGravityMultiplier;
-            }
-            
-            // If we start to fall, we aren't jumping anymore
-            if(_yDirection > 0)
-            {
-                if(_rb.linearVelocityY <= 0)
-                {
-                    _isJumping = false;
-                } 
-            }
-            else
-            {
-                if(_rb.linearVelocityY >= 0)
-                {
-                    _isJumping = false;
-                } 
-            }
-        }
+        _flipY = flip ? -1f : 1f;
+        transform.localScale = new Vector3(FacingDirection, _flipY, 1f);
     }
 
     private void UpdateGroundCheck()
     {
-        Bounds bounds = _collider.bounds;
-        Vector2 checkOrigin = new Vector2(bounds.center.x, bounds.center.y - bounds.extents.y * _yDirection);
+        Bounds bounds = Collider.bounds;
+        Vector2 checkOrigin = new Vector2(bounds.center.x, bounds.center.y - bounds.extents.y * _flipY);
         Vector2 checkSize = new Vector2(bounds.size.x - 0.05f, _groundCheckHeight);
 
         RaycastHit2D hit = Physics2D.CapsuleCast(
@@ -170,13 +153,75 @@ public sealed class PlayerMovement : MonoBehaviour
         {
             Landed();
         }
-        
         IsGrounded = true;
+    }
+    
+    private void UpdateObstacleCheck()
+    {
+        Bounds bounds = Collider.bounds;
+        float originYStart = _flipY > 0 ? bounds.min.y : bounds.max.y; 
+        Vector2 origin = new Vector2(
+            bounds.center.x + bounds.extents.x * FacingDirection,
+            originYStart + bounds.size.y * _obstacleCheckHeight * _flipY
+        );
+
+        RaycastHit2D hit = Physics2D.Raycast(
+            origin, FacingDirection * Vector2.right, _obstacleCheckDistance, _obstacleLayer
+        );
+
+        if(Obstacle is null && hit.collider is not null)
+        {
+            Obstacle = hit.collider.gameObject;
+            IsObstacleInFront = true;
+            OnObstacleTouched?.Invoke(Obstacle);
+        }
+        else if(IsObstacleInFront && hit.collider is null)
+        {
+            IsObstacleInFront = false;
+            OnObstacleReleased?.Invoke(Obstacle);
+            Obstacle = null;
+        }
+    }
+    
+    private void UpdateMovement()
+    {
+        _rb.linearVelocityX = _movementInput * _movementSpeed;
+    }
+
+    private void UpdateJump()
+    {
+        if(!_isJumping) return;
+
+        float jumpHeightSinceStart = transform.position.y - _jumpStartY * _flipY;
+        if(jumpHeightSinceStart >= _minJumpHeight)
+        {
+            if (!_jumpHeld)
+            {
+                _rb.linearVelocityY *= _jumpEndGravityMultiplier;
+            }
+            
+            // If we start to fall, we aren't jumping anymore
+            if(_flipY > 0)
+            {
+                if(_rb.linearVelocityY <= 0)
+                {
+                    _isJumping = false;
+                } 
+            }
+            else
+            {
+                if(_rb.linearVelocityY >= 0)
+                {
+                    _isJumping = false;
+                } 
+            }
+        }
     }
 
     private void Landed()
     {
         _isJumping = false;
         _coyoteTimer = 0;
+        OnLanded?.Invoke();
     }
 }
